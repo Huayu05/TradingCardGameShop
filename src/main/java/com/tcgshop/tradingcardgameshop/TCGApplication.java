09e4e1b8855e63101f463fa4b790d8880c36d338
Ong Hua Yu <onghuayu5b@gmail.com>
2025-03-15 14:11:41 +0800

    Scene Switching and Time Delay Test Done

--- src/main/java/com/tcgshop/tradingcardgameshop/TCGApplication.java
+++ src/main/java/com/tcgshop/tradingcardgameshop/TCGApplication.java
@@ -1,22 +1,58 @@
 package com.tcgshop.tradingcardgameshop;
 
+import javafx.animation.PauseTransition;
+import javafx.util.Duration;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class TCGApplication extends Application {
+    private Stage primaryStage;
+
     @Override
-    public void start(Stage stage) {
-        HelloPane grid = new HelloPane();
-        Scene scene = new Scene(grid);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        changeWindowOne();
     }
 
     public static void main(String[] args) {
         launch();
+    }
+
+    public void changeWindowOne() {
+        Label label = new Label("'A Login Page'");
+        Button button = new Button("Login");
+        button.setOnAction(e -> {
+            PauseTransition pause = new PauseTransition(Duration.seconds(2));
+            pause.setOnFinished(event -> {
+                changeWindowTwo();
+            });
+            pause.play();
+        });
+
+        Scene scene = new Scene(new VBox(label, button), 300, 200);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public void changeWindowTwo() {
+        Label label = new Label("Welcome to the Trading Card Game Shop!");
+        Button button = new Button("Logout");
+        button.setOnAction(e -> {
+            changeWindowOne();
+        });
+
+        VBox root = new VBox(label, button);
+        root.setStyle("-fx-background-color: lightblue;");
+        Scene scene = new Scene(root, 300, 200);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
