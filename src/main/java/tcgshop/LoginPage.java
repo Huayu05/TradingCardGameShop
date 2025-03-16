d04a06716c2b70b68031964505031fb2654f09dc
Ong Hua Yu <onghuayu5b@gmail.com>
2025-03-16 21:32:54 +0800

    SignIn part partially done

--- src/main/java/tcgshop/LoginPage.java
+++ src/main/java/tcgshop/LoginPage.java
@@ -5,34 +5,41 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.*;
 
 class LoginPage extends Scene {
+    // Class Attribute
+    private Image bgImage;
+    private ImageView bgImageView;
+    private StackPane loginMain;
+
     // Constructor
     public LoginPage() {
         // Call constructor from parent class
         super(new StackPane(), 1000, 600);
 
         // StackPane setup
-        StackPane loginPageBase = (StackPane) getRoot();
+        StackPane loginPageRoot = (StackPane) getRoot();
 
         // Background Image
-        Image image = new Image(getClass().getResource("/tcgshop/images/pokeball_background_picture.png").toExternalForm());
-        ImageView imageView = new ImageView(image);
-        imageView.setPreserveRatio(false);
-        imageView.fitWidthProperty().bind(loginPageBase.widthProperty());
-        imageView.fitHeightProperty().bind(loginPageBase.heightProperty());
-        loginPageBase.getChildren().addFirst(imageView);
+        bgImage = new Image(getClass().getResource("/tcgshop/images/pokeball_background_picture.png").toExternalForm());
+        bgImageView = new ImageView(bgImage);
+        bgImageView.setPreserveRatio(false);
+        bgImageView.fitWidthProperty().bind(loginPageRoot.widthProperty());
+        bgImageView.fitHeightProperty().bind(loginPageRoot.heightProperty());
+        loginPageRoot.getChildren().addFirst(bgImageView);
 
-        Pane paneOne = new Pane();
-        paneOne.setStyle("-fx-background-color: #FFFDE7;-fx-background-radius: 20px;");
-        paneOne.setMinSize(600, 360);
-        paneOne.setMaxSize(900, 540);
+        // Main Login and Signin Pane
+        loginMain = new StackPane();
+        loginMain.setStyle("-fx-background-color: #FFFDE7;-fx-background-radius: 20px;");
+        loginMain.setMinSize(600, 360);
+        loginMain.setMaxSize(900, 540);
+        loginMain.setPadding(new Insets(20));
+        loginPageRoot.getChildren().add(loginMain);
+        StackPane.setMargin(loginMain, new Insets(20));
 
-        loginPageBase.getChildren().add(paneOne);
-        StackPane.setMargin(paneOne, new Insets(20));
-
-
+        // Signin Pane
+        SignInPane signInPane = new SignInPane();
+        loginMain.getChildren().add(signInPane);
     }
 }
--- src/main/java/tcgshop/SignInPane.java
+++ src/main/java/tcgshop/SignInPane.java
@@ -1,10 +1,41 @@
 package tcgshop;
 
-import javafx.scene.layout.VBox;
+import javafx.geometry.Insets;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.*;
 
-public class SignInPane extends VBox {
+public class SignInPane extends GridPane {
     // Constructor
     public SignInPane() {
+        // Call constructor from parent class
+        super();
+
+        Pane blank = new Pane();
+        VBox signInVBox = new VBox();
+        this.add(blank, 0, 0);
+        this.add(signInVBox, 1, 0);
+        ColumnConstraints col1 = new ColumnConstraints();
+        col1.setPercentWidth(40);
+        ColumnConstraints col2 = new ColumnConstraints();
+        col2.setPercentWidth(60);
+        col1.setHgrow(Priority.ALWAYS);
+        col2.setHgrow(Priority.ALWAYS);
+        this.getColumnConstraints().addAll(col1, col2);
+
+        this.setPadding(new Insets(20));
+        this.setStyle("-fx-background-color: white;");
+
+
+        Label topic = new Label("Create an Account");
+        TextField username = new TextField();
+        PasswordField password = new PasswordField();
+        ChoiceBox<String> role = new ChoiceBox<String>();
+        role.getItems().addAll("User", "Admin");
+        signInVBox.getChildren().addAll(topic, username, password, role);
+
 
     }
 }
